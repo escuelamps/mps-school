a233d48b86ca90e6a2b3cd4de1b3479497a992e4
Sammy Cuartas <cuartascortes@gmail.com>
2026-07-15 08:49:53 -0500

    Hardcode Firebase public config for Vercel

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBLbVAmpri8BRRlA98MoP-I7i4wjZslQ28",
+  authDomain: "mps-school-bf7ed.firebaseapp.com",
+  projectId: "mps-school-bf7ed",
+  storageBucket: "mps-school-bf7ed.firebasestorage.app",
+  messagingSenderId: "742759520223",
+  appId: "1:742759520223:web:acf20e11de1a1e21de966a",
+  measurementId: "G-3GBQL0BZG1"
 };
 
 // Initialize Firebase only if it hasn't been initialized already (useful for Next.js hot reloading)
